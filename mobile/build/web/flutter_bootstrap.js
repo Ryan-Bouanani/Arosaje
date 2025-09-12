71b0558031dc9a612f1f1d27589dc17d3cfc1f16
Ryan-Bouanani <93480946+Ryan-Bouanani@users.noreply.github.com>
2025-09-12 19:54:20 +0200

    feat: rebuild Flutter web with all dotenv fixes applied

--- mobile/build/web/flutter_bootstrap.js
+++ mobile/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2130372023"
+    serviceWorkerVersion: "2710338113"
   }
 });
